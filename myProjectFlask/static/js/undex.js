1efa71d83d22dfd1711834284c7791e879b867e5
osezelej <2osezelejoseph@gmail.com>
2022-11-04 14:19:24 +0100

    OOP class

--- myProjectFlask/static/js/undex.js
+++ myProjectFlask/static/js/undex.js
@@ -1,32 +1,61 @@
-let numRows = 10;
-let outerArr = [];
-let innerArray = [];
-for (let i = 1; i <= numRows; i++){
-    if (i == 1){
-        innerArray = [1]
-    }else if (i == 2 ){
-        innerArray = [1, 1]
-    }else{
-        let start = 0
-        for(let w=1; w <=i; w++){
-            if (w == 1){
-                innerArray.push(w);
-            }else if (w == i){
-                innerArray.push(1) 
-            }else{
-                let arr = outerArr[i - 2];
-                num = 0
-                for(let s= start; s < w; s++){
-                    start ++
-                    num += arr[s]
-                }
-                start -= 1
-                innerArray.push(num)
+// '()[]{}'
+// let brackets = '{';
+// let start = 0;
+// let answer = true
+// if (brackets){
+//     for(let i = 0; i < brackets.length; i+= 2){
+//         console.log(brackets[i])
+//         if (brackets[i] == '('){
+//             if(brackets[i + 1] == ')'){
+//                 continue;
+//             }else{
+//                 answer = false
+//                 break;}
+//         }else if (brackets[i] == '['){
+//             if(brackets[i + 1] == ']'){
+//                 continue;
+//             }else{
+//                 answer = false
+//                 break;
+//             }
+//         }else if (brackets[i] == '{'){
+//             if(brackets[i + 1] == '}'){
+//                 continue;
+//             }else{
+//                 answer = false;
+//                 break;
+//             }
+//         }else{
+//             answer = false
+//             break;
+//         }
+//     }
+// }else{
+//     answer = false;
+// }
+// console.log(answer)
 
-            }
-        }
-    }
-    outerArr.push(innerArray)
-    innerArray = []
-} 
-console.log(outerArr)
+// functional programming 
+
+function callName(name){
+    let n = name.toUpperCase()
+   return n
+}
+
+var message = callName('vincent')
+console.log(message)
+
+
+// add, sub, mul, div
+function add(n1, n2){
+    let answer = n1 + n2;
+    return answer
+}
+
+function sub(n1, n2){
+    let answer = n1 - n2;
+    return answer
+}
+
+
+console.log(message)
--- myProjectFlask/test.py
+++ myProjectFlask/test.py
@@ -1,39 +1,72 @@
-n = 5
-answer = []
-if n > 0:
-    for i in range(n):
-        inarr = []
-        answer.append(inarr)
+# n = 5
+# answer = []
+# if n > 0:
+#     for i in range(n):
+#         inarr = []
+#         answer.append(inarr)
 
-    print(answer)
+#     print(answer)
 
-    num = 0
-    for i in range(n):
-        if i == 0:
-            for w in range(1,n +1):
-                num = w
-                answer[i].append(w)
-        else:
-            num += 1 
-            answer[i].append(num)
+#     num = 0
+#     for i in range(n):
+#         if i == 0:
+#             for w in range(1,n +1):
+#                 num = w
+#                 answer[i].append(w)
+#         else:
+#             num += 1 
+#             answer[i].append(num)
 
-    last_item = answer[-1]
-    index = -1
-    for i in range(n-1):
-        num += 1
-        answer[-1].insert(index-1, num)
-        index -= 1
+#     last_item = answer[-1]
+#     index = -1
+#     for i in range(n-1):
+#         num += 1
+#         answer[-1].insert(index-1, num)
+#         index -= 1
 
 
-    data = answer[-n + 1:-1]
-    data.reverse()
-    for i in range(len(data)):
-        for c in range(n-1):
-            num += 1
-            data[i].insert(c, num)
+#     data = answer[-n + 1:-1]
+#     data.reverse()
+#     for i in range(len(data)):
+#         for c in range(n-1):
+#             num += 1
+#             data[i].insert(c, num)
             
-    data.reverse()
-    answer[-n + 1:-1] = data
-    print(answer)
-else:
-    print(None)
+#     data.reverse()
+#     answer[-n + 1:-1] = data
+#     print(answer)
+# else:
+#     print(None)
+
+
+class Car:
+    def __init__(self, brand, color, no_of_tyres):
+        self.brand = brand
+        self.color = color
+        self.no_of_tyres = no_of_tyres
+        
+    def move(self):
+        return f'{self.no_of_tyres} tyres is rolling..., {self.brand} is moving'
+    
+    def stop(self):
+        return f'{self.no_of_tyres} tyres has stoped!, {self.brand} has stoped'
+    
+
+
+ferrari = Car(brand='ferrari', color='red', no_of_tyres='4')
+print(ferrari.brand)
+print(ferrari.move())
+ferrari.brand = 'ferrari newest model'
+ferrari.typeOfCar = 'sportCar'
+print(ferrari.move())
+print(ferrari.typeOfCar)
+
+class SportCar(Car):
+    def __init__(self, brand, color, no_of_tyres, no_of_doors):
+        super().__init__(brand, color, no_of_tyres)
+        self.no_of_doors = no_of_doors
+    
+
+bugattiVeron = SportCar('bugatti', color='black', no_of_tyres=4, no_of_doors=2)
+    
+print(bugattiVeron.move())
